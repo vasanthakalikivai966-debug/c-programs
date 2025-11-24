1fbf5edf2873d6ad1c7884506e6e8f6f2587f73e
vasanthakalikivai966-debug <vasanthakalikivai966@gmail.com>
2025-11-24 21:25:36 +0530

    Add files via upload

---
+++ Square root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double num,root;
+	//Input
+	printf("Enter an integer:");
+	scanf("%lf",&num);
+	root=sqrt(num);
+	printf("The Square Root of %.2lf is %.2lf.",num,root);
+	return 0;
+}
