8bf91419fce2967860d0d7f69e769bb60754b437
vasanthakalikivai966-debug <vasanthakalikivai966@gmail.com>
2025-11-24 21:26:00 +0530

    Add files via upload

---
+++ square,cube(n)[1].c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the value of n");
+	scanf("%d",&n);
+	int square =n*n;
+	int cube =n*n*n;
+	printf("the square of n is :%d",square);
+	printf("the cube of n is :%d", cube);
+	return 0;
+}
