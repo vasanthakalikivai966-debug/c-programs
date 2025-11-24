e53e60ceef3880c09f4ddf28d7c3981e988b6f36
vasanthakalikivai966-debug <vasanthakalikivai966@gmail.com>
2025-11-24 21:27:39 +0530

    Add files via upload

---
+++ Transposematrix.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],t[10][10],i,j,r,c;
+	printf("Enter rows and columns:");
+	scanf("%d%d",&r,&c);
+	printf("Enter elements into A matrix:");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			t[i][j]=a[i][j];
+		}
+	}
+	printf("transpose matrix is\n");
+	for(i=0;i<c;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%d\t",t[i][j]);
+		}
+		printf("\n");
+	}
+}
