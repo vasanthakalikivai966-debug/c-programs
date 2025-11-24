34ee991eeb37c41aae096b556b088f2ee8c0209a
vasanthakalikivai966-debug <vasanthakalikivai966@gmail.com>
2025-11-24 21:26:54 +0530

    Add files via upload

---
+++ Sum of Matrix.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],b[10][10],c[10][10],i,j;
+	//read matrix A
+	printf("Enter the elements of A matrix:");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	//print the matrix A
+	printf("\n The elements of A matrix:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	//read matrix B
+	printf("Enter the elements of B matrix:");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	//print the matrix B
+	printf("\n The elements of B matrix:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	//logic of sum of matrix A and B
+	printf("Logic of sum of two matrix:");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			c[i][j]=a[i][j]+b[i][j];
+		}
+	}
+			//print the matrix C
+	printf("\n The elements of C matrix:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",c[i][j]);
+		}
+		printf("\n");
+    }
+	
+}
